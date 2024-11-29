8c407af38d2b640220b924ab97d819cf25f92662
Ermand Haruni <88191097+ErmandH@users.noreply.github.com>
2024-11-29 21:55:00 +0300

    small fix

--- BoilerPlate.AdminPanel/Controllers/HomeController.cs
+++ BoilerPlate.AdminPanel/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace BoilerPlate.AdminPanel.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         [HttpGet("")]
